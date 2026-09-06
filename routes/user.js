958eaab3db844f0130705cf6509228a73a52264a
sanjeetp610-netizen <sanjeetp610@gmail.com>
2026-09-06 23:35:53 +0530

    Update user middleware import

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const passport = require("passport");
-const { saveRedirectUrl } = require("../middleware.js");
+const { saveRedirectUrl } = require("../appMiddleware.js");
 
 const userController = require("../controllers/users.js");
 
