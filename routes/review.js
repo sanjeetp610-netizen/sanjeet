e543175530bee926f57bd652a3f0db2c4815eab6
sanjeetp610-netizen <sanjeetp610@gmail.com>
2026-09-07 18:29:34 +0530

    Add review editing route

--- routes/review.js
+++ routes/review.js
@@ -1,24 +1,27 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 const wrapAsync = require("../utils/wrapAsync.js");
-const ExpressError = require("../utils/ExpressError.js");
-const Listing = require("../models/listing.js");
 const { validateReview, isLoggedIn, isReviewAuther } = require("../appMiddleware.js");
 
 const reviewControllers = require("../controllers/reviews.js");
-const review = require("../models/review.js");
 
-//Post Route
 router.post("/",
     isLoggedIn,
     validateReview,
     wrapAsync(reviewControllers.createReview)
 );
 
-//Delete Reviews Route
+router.put("/:reviewId",
+    isLoggedIn,
+    validateReview,
+    isReviewAuther,
+    wrapAsync(reviewControllers.updateReview)
+);
+
 router.delete("/:reviewId",
     isLoggedIn,
     isReviewAuther,
-    wrapAsync(reviewControllers.destroyReview));
+    wrapAsync(reviewControllers.destroyReview)
+);
 
 module.exports = router;
